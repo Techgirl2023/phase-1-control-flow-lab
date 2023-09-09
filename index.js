488842eecd473d3236494cfb8d9348bd14e5e0a6
Ashanti Logan <Ashantilogan2@gmail.com>
2023-09-09 19:10:00 -0400

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance){
+     if (rideDistance <= 400) {
+         return 'This one is on me!'  
+     } 
+     if (rideDistance > 400 && rideDistance < 2000){
+         return 'That will be twenty bucks.'
+     }
+     if (rideDistance >= 2500){
+         return 'No can do.'
+     }
+     if (rideDistance > 2000){
+        return 'I will gladly take your thirty bucks.'
+     }
+     }
+const city = 'NYC';
+function ternaryCheckCity(city) {
+return (city === 'NYC') ? `Ok, sounds good.` : `No go.`;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+const tip = 'generous';
+function switchOnCharmFromTip (tip) {
+switch (tip) {
+     case 'generous':
+     return ('Thank you so much.');
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+     case 'not as generous':
+     return ('Thank you.');
+
+     default:
+     return ('Bye.');
+}
 }
